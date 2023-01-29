c260583ba4966950ea6c2087ca439740b839ec3f
Janice Cotcher <42756980+janice-cotcher@users.noreply.github.com>
2023-01-29 22:03:16 +0000

    Initial commit

---
+++ 03_Practice.py
@@ -0,0 +1,4 @@
+# Save your full name as a variable and find the number of characters. Print 
+# out a statement with your name and the number of characters like the 
+# following:
+# My name is Janice Cotcher and I have 14 characters in my name.
